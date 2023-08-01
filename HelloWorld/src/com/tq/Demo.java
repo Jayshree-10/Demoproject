93359610edf48962232767b1051f4095459ca8d4
Jayshree-10 <tqjayshreec@gmail.com>
2023-08-01 17:18:16 +0530

    basic demo

---
+++ HelloWorld/src/com/tq/Demo.java
@@ -0,0 +1,10 @@
+package com.tq;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi all , welcome to Git session");
+	}
+
+}
