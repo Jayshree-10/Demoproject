1976c1fd93fde0dd55c8e30613d36ffbab9fcdfb
Jayshree-10 <tqjayshreec@gmail.com>
2023-08-01 17:22:08 +0530

    sayHi(String str) method added

--- HelloWorld/src/com/tq/Demo.java
+++ HelloWorld/src/com/tq/Demo.java
@@ -2,9 +2,15 @@ package com.tq;
 
 public class Demo {
 
+	public String sayHi(String name)
+	{
+		return "Hello "+name;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi all , welcome to Git session");
+		Demo d=new Demo();
+		d.sayHi("Jayshree Chaudhary");
 	}
 
 }
